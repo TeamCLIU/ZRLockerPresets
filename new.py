f9e0740633d9b580b22d9c4e8c208e0b1b5c128f
Creaffy <81998541+creaffy@users.noreply.github.com>
2023-01-03 19:20:51 +0100

    Update new.py

--- new.py
+++ new.py
@@ -1,7 +1,7 @@
 # >>> NEW PRESET CREATOR FUNCTION
-# !!! MAKE SURE YOU DON'T ALREADY HAVE A PRESET SAVED AS "preset.json" TO AVOID OVERWRITING
+# !!! MAKE SURE YOU DON'T ALREADY HAVE A PRESET SAVED AS "new_preset.json" TO AVOID OVERWRITING
 import os
 
-file = open(f"{os.path.realpath(os.path.dirname(__file__))}/presets/preset.json", "w")
+file = open(f"{os.path.realpath(os.path.dirname(__file__))}/presets/new_preset.json", "w")
 file.writelines(['{ "outfit": 0, "backpack": 0, "melee": 0, "parachute": 0 }'])
 file.close()
