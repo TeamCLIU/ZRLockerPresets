2fdecb358c3564106802daa109356383b45abd82
creaffy <81998541+creaffy@users.noreply.github.com>
2023-01-06 10:42:52 +0100

    Emotes and Sprays support

--- new.py
+++ new.py
@@ -1,7 +1,15 @@
 # >>> NEW PRESET CREATOR FUNCTION
 # !!! MAKE SURE YOU DON'T ALREADY HAVE A PRESET SAVED AS "new_preset.json" TO AVOID OVERWRITING
-import os
+import json, os
 
-file = open(f"{os.path.realpath(os.path.dirname(__file__))}/presets/new_preset.json", "w")
-file.writelines(['{ "outfit": 0, "backpack": 0, "melee": 0, "parachute": 0 }'])
-file.close()
+preset = {
+    "outfit": 0,
+    "backpack": 0,
+    "melee": 0,
+    "parachute": 0,
+    "emotes": [0,0,0,0,0,0],
+    "sprays": [0,0,0,0]
+}
+
+with open(f"{os.path.realpath(os.path.dirname(__file__))}/presets/new_preset.json", "w") as file:
+    file.write(json.dumps(preset))
